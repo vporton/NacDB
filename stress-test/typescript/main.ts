c0a2032fc633f0cfc84dc60a9e5d080924dec02f
Victor Porton <porton.victor@gmail.com>
2023-07-18 09:29:23 +0300

    code for stress testing

--- stress-test/typescript/main.ts
+++ stress-test/typescript/main.ts
@@ -1,7 +1,18 @@
 import { Worker } from 'worker_threads';
 // import * as _ from 'lodash';
+import { DBOptions, MoveCap } from "../../example/src/declarations/index/index.did.js";
+import { idlFactory } from '../../example/src/declarations/index';
+
+const dbOptions: DBOptions = {
+  moveCap: {'usedMemory': BigInt(500_000)},
+  hardCap: [BigInt(1000)],
+  newPartitionCycles: BigInt(300_000_000_000),
+  constructor: async opts => await idlFactory(opts),
+};
+const indexCanister = Index(dbOptions);
 
 function runThread() {
+
   const worker = new Worker('./worker.js', { workerData: {} });
 
   worker.on('message', (response) => {
@@ -13,9 +24,9 @@ function runThread() {
   });
 
   worker.on('exit', (code) => {
-    // if (code !== 0) {
+    if (code !== 0) {
       console.error(`Worker stopped with exit code ${code}`);
-    // }
+    }
   });
 }
 
